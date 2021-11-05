574ac74d0772e6cac5bdcb3c017ec267c92ffe11
Kakha Gujejiani <67228459+Gujejiani@users.noreply.github.com>
2021-11-05 22:23:31 +0400

    changed

--- myApp.js
+++ myApp.js
@@ -3,8 +3,8 @@ var app = express();
 
 app.get('/', (req, res)=>{
     console.log(__dirname)
-    res.sendFile(`${__dirname}/views/index.html`)
-    // res.send('Hello Expresss')
+    // res.sendFile(`${__dirname}/views/index.html`)
+     res.send('Hello Expresss')
 })
 
 

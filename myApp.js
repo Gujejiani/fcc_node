2e5f0fd8a7e8ac598d1bb58153cccca3bdd40c38
Kakha Gujejiani <67228459+Gujejiani@users.noreply.github.com>
2021-11-07 20:20:45 +0400

    Use body-parser to Parse POST Requests

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 require('dotenv').config()
 
 
-app.use(bodyParser({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(bodyParser.json())
 
